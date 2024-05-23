2bf8bb54b277de003af225a2451296cd8c8ae382
Minh Truong <ldcc.truongtran@lotte.vn>
2024-05-23 16:59:07 +0700

    done animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
+import { FormLogin } from "@/components/Login/FormLogin";
 import Login from "@/components/Login/Login";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-1/2 h-full -m-2" style={{
-    
-    }}> 
-      <Login></Login>
+    <div className="w-1/2 h-full -m-2" >
+      <div >
+        <Login></Login>
+      </div>
+      <div
+        style={{ position: "absolute", top: "0px", right: "20%" }}
+        className="mt-20"
+      >
+        <FormLogin />
+      </div>
     </div>
   );
 }
