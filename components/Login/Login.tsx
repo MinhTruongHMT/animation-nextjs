4712720622bb3a1b545685e0c2b16d1310d7efe2
Minh Truong <ldcc.truongtran@lotte.vn>
2024-05-23 15:48:58 +0700

    sdjsh

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,11 +1,13 @@
 "use client";
-import Lottie from "lottie-web";
+// import Lottie from "lottie-web";
 import { createRef, useEffect } from "react";
 import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./Login.module.css";
+import animationData from "@/public/Privacy/data.json";
+import Lottie from "react-lottie";
 
 const settings = {
   autoplaySpeed: 3500,
@@ -22,37 +24,40 @@ export default function Login() {
   let animationContainer = createRef();
   let animationContainer1 = createRef();
 
- 
-  useEffect(() => {
-    const anima = Lottie.loadAnimation({
-      container: animationContainer.current,
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      path: "/Discover/data.json",
-    });
-    const anima1 = Lottie.loadAnimation({
-      container: animationContainer1.current,
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      path: "/Share/data.json",
-    });
+  // useEffect(() => {
+  //   const anima = Lottie.loadAnimation({
+  //     container: animationContainer.current,
+  //     renderer: "svg",
+  //     loop: true,
+  //     autoplay: true,
+  //     path: "/Discover/data.json",
+  //   });
+  //   const anima1 = Lottie.loadAnimation({
+  //     container: animationContainer1.current,
+  //     renderer: "svg",
+  //     loop: true,
+  //     autoplay: true,
+  //     path: "/Share/data.json",
+  //   });
 
-    return () => {
-      anima.destroy();
-      anima1.destroy();
+  //   return () => {
+  //     anima.destroy();
+  //     anima1.destroy();
 
-    };
-  });
+  //   };
+  // });
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    renderer: "svg",
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
-    <div className={styles.login}>
-      <Slider {...settings}>
-       
-        <div ref={animationContainer}></div>
-        <div ref={animationContainer1}></div>
-        
-      </Slider>
+    <div>
+      <Lottie options={defaultOptions} height={400} width={400} />
     </div>
   );
 }
